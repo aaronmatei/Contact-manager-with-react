4d5b5ac659a539525ef7ef4ca260eb178f1bcb78
aaronmatei <aronique@gmail.com>
2019-10-10 00:33:27 +0300

    Updates

---
+++ precache-manifest.d954bbb13c7a36bfa20755f8ab13467c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c2edf70ea4556528e527d6715ded5f5",
+    "url": "/Contact-manager-with-react/index.html"
+  },
+  {
+    "revision": "24b128300d5466ce9765",
+    "url": "/Contact-manager-with-react/static/css/2.f2688b6b.chunk.css"
+  },
+  {
+    "revision": "116eb26cc18d41c281a4",
+    "url": "/Contact-manager-with-react/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "24b128300d5466ce9765",
+    "url": "/Contact-manager-with-react/static/js/2.b4dff567.chunk.js"
+  },
+  {
+    "revision": "116eb26cc18d41c281a4",
+    "url": "/Contact-manager-with-react/static/js/main.8388f37a.chunk.js"
+  },
+  {
+    "revision": "5a9e92cfba75002525bd",
+    "url": "/Contact-manager-with-react/static/js/runtime~main.4b9ad525.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Contact-manager-with-react/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Contact-manager-with-react/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Contact-manager-with-react/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Contact-manager-with-react/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Contact-manager-with-react/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
